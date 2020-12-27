ceba2afc038447bd2a38aa3f45270ee380682fe4
MoazzamAzeemi <38378900+MoazzamAzeemi@users.noreply.github.com>
2020-12-27 23:54:29 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2>Edited by Moazzam</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edited <code>src/App.js</code> and pushed to github.
         </p>
         <a
           className="App-link"
--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+function Hi({name, age}) {
+  return <div>
+            <h1>React App</h1>
+            <p>Hello from <strong>{name}</strong></p>
+            <p>The number <strong>{age}</strong> is passed through <code>"props"</code></p>
+            <hr></hr>
+            <h3>Unordered List</h3>
+            <ul>
+              <li>one</li>
+              <li>two</li>
+              <li>three</li>
+              <li>four</li>
+            </ul>
+            <hr></hr>
+            <p>When we add 5 and 3 through JS expression, answer is following:</p>
+            <h4>{5+8}</h4>
+         </div>
+}
+
+ReactDOM.render(<Hi name="Moazzam Shah" age={40} />, document.querySelector('#root'));
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
